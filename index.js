db32e736e36f82c98bbf654c6737e476cad6e731
oliwermazur <oliwer.mazur4@gmail.com>
2024-01-07 20:55:52 +0100

    frontend

---
+++ index.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([53.4285, 14.5528], 13);
+
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    maxZoom: 19,
+    attribution: 'Map tiles by Carto, under CC BY 3.0. Data by OpenStreetMap, under ODbL.'
+}).addTo(map);
